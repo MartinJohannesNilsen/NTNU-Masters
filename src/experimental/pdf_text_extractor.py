ecf16d904f807051948828743b252fe381397213
OleJonas <jonas.liahagen@hotmail.com>
2023-01-26 14:23:32 +0100

    Added some more pdfs with shooters' words. Also cleaned up in pdf_text_extractor.py

--- src/experimental/pdf_text_extractor.py
+++ src/experimental/pdf_text_extractor.py
@@ -1,26 +1,27 @@
 # importing required modules
-from pathlib import Path, PureWindowsPath
-from tika import parser
+from pathlib import Path
 from PyPDF2 import PdfReader
-import glob
-import csv
+import unicodedata
+import re
 
 root_folder = Path(__file__).resolve().parents[2]
 src_folder = Path(__file__).resolve().parents[1]
 
+# Path to pdfs containing school shooters' texts
 doc_path = Path(Path(__file__).resolve().parents[2]/"schoolshootersinfo")
-print(doc_path)
-#doc_path = f"{doc_path}/**/*.pdf"
-print(doc_path)
 
 files = doc_path.glob("**/*.pdf")
-print(files)
+print(f"Found these pdf files at {doc_path}:\n{files}")
 
 for file in files:
     reader = PdfReader(str(file.resolve()))
-    fname = file.name
-    fpath = src_folder/"raw_text"/fname
+    fname = file.stem
+    fpath = src_folder/"raw_text"/f"{fname}.txt"
 
-    with open(fpath, "w+") as f:
+    with open(fpath, "w+", encoding="utf-8") as f:
         for page in reader.pages:
-            f.write(page.extract_text())
+            text = page.extract_text()
+            text = "".join(c if unicodedata.category(c) != "Co" else " " for c in text).strip() # Remove unwanted special chars and encode to utf-8
+            text = re.sub(r'(-+)', '', text) # Some texts contain ----- as separators. Remove these separators
+            f.write(text)
+        
