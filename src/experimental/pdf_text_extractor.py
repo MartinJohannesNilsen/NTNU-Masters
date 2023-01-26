5d87ee3a92bd168e3c99000dd0e273ddbf73a1fa
OleJonas <jonas.liahagen@hotmail.com>
2023-01-26 11:07:23 +0100

    Please permission wtf man fix please no sense ye

--- src/experimental/pdf_text_extractor.py
+++ src/experimental/pdf_text_extractor.py
@@ -1,17 +1,26 @@
 # importing required modules
 from pathlib import Path, PureWindowsPath
 from tika import parser
+from PyPDF2 import PdfReader
 import glob
+import csv
 
-#print(__file__)
+root_folder = Path(__file__).resolve().parents[2]
+src_folder = Path(__file__).resolve().parents[1]
 
-doc_path = Path(f"{__file__}/../../schoolshootersinfo").resolve()
+doc_path = Path(Path(__file__).resolve().parents[2]/"schoolshootersinfo")
 print(doc_path)
-doc_path = f"{doc_path}/**/*.pdf"
+#doc_path = f"{doc_path}/**/*.pdf"
 print(doc_path)
 
-files = glob.glob(doc_path, recursive=True)
+files = doc_path.glob("**/*")
 print(files)
 
 for file in files:
-    print(file)
+    reader = PdfReader(str(file.resolve()))
+    fname = file.name
+    fpath = src_folder/"raw_text"/fname
+
+    with open(fpath, "w+") as f:
+        for page in reader.pages:
+            f.write(page.extract_text())
