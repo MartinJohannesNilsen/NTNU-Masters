1598d7d8aefb4a7924ab322be474fe4df10a1945
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2023-01-26 14:54:37 +0100

    data from new perpetrator

--- src/experimental/pdf_text_extractor.py
+++ src/experimental/pdf_text_extractor.py
@@ -4,6 +4,6 @@ from pathlib import Path
 doc_path = Path(__file__).resolve().parents[2] / "schoolshootersinfo"
 print("Path:", doc_path)
 
-files = (doc_path).rglob("*.pdf")
+files = doc_path.rglob("*.pdf")
 for path in files:
     print(path)
