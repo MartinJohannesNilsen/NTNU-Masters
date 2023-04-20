ce7f0ca1e668c4d3081d8c3d93cf13db73d8f833
Martijni <martinjnilsen@icloud.com>
2023-04-20 09:51:05 +0200

    Testing sklearn

--- src/experiments/models/test/test_svm.py
+++ src/experiments/models/test/test_svm.py
@@ -10,7 +10,7 @@ from experiments.utils.metrics import get_metrics, print_metrics_comprehensive
 from experiments.features.load_and_store_emb_batches import read_h5
 
 
-def test_embeddings(embedding_type = "bert"):
+def test_embeddings(embedding_type = "bert", batch_size: int = None):
     assert embedding_type in ["glove", "fasttext", "bert"], "Embedding not supported!"
     data_name = f"test_sliced_stair_twitter_{embedding_type}.h5"
     # data_name = f"hold_out_test_sliced_stair_twitter_{embedding_type}.h5"
@@ -22,18 +22,30 @@ def test_embeddings(embedding_type = "bert"):
 
     # Get pickled dataframe with embeddings
     data_path = Path(os.path.abspath(__file__)).parents[2] / "features" / "embeddings" / data_name
-    data = read_h5(data_path)
-    X = np.array([element.ravel().tolist() for element in data["emb_tensor"]])
-    y = np.array(data["label"])
 
-    return model.predict(X), y
+    if not batch_size:
+        data = read_h5(data_path)
+        X = np.array([element.ravel().tolist() for element in data["emb_tensor"]])
+        y = np.array(data["label"])
+        y_pred = model.predict(X)
+    else:
+        n_total_samples = len(read_h5(data_path, col_name="idx"))
+        y_pred = []
+        for i in range(0, n_total_samples, batch_size):
+            data = read_h5(data_path, start=i, chunk_size=batch_size)
+            X = np.array([element.ravel().tolist() for element in data["emb_tensor"]])
+            y = np.array(data["label"])
+            y_pred.extend(model.predict(X))
+
+    return y_pred, y
 
 
 if __name__ == "__main__":
-    embedding_type = "bert" # "glove", "fasttext", "bert"
-    print(embedding_type)
-    y_pred, y_true = test_embeddings(embedding_type)
-    threshold = 0.75
-    out = [1 if pred > threshold else 0 for pred in y_pred]
-    stats = get_metrics(out, y_true)
-    print_metrics_comprehensive(stats)
+    embedding_types = ["glove", "fasttext", "bert"]
+    for embedding_type in embedding_types:
+        print(embedding_type)
+        y_pred, y_true = test_embeddings(embedding_type, batch_size=200)
+        threshold = 0.5
+        out = [1 if pred > threshold else 0 for pred in y_pred]
+        stats = get_metrics(out, y_true)
+        print_metrics_comprehensive(stats)
