b03a92c2dc7cf8b271c8acce8ff05e7e21e0329a
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2023-04-20 13:40:45 +0200

    LIWC

--- src/dataset_creation/create_xlsx_liwc.py
+++ src/dataset_creation/create_xlsx_liwc.py
@@ -1,57 +1,11 @@
 from csv import QUOTE_NONE, field_size_limit
-from functools import partial
 import os
 from pathlib import Path
-from typing import List
-import click
 import pandas as pd
 import sys
 field_size_limit(sys.maxsize)
-
-
-def append_date_columns(dataframe: pd.DataFrame):
-    df = dataframe.copy()
-    date_dict = { "year": [], "month": [], "day": [] }
-    for date in df["date"].convert_dtypes():
-        year, month, day = [None, None, None] if type(date) == pd._libs.missing.NAType else (date.split("-") + 3 * [None])[:3]
-        date_dict["year"].append(year)
-        date_dict["month"].append(month)
-        date_dict["day"].append(day)
-    for key in date_dict.keys():
-        df[key] = date_dict[key]
-
-    # Need to format some of the columns to types
-    df["date"] = pd.to_datetime(df['date'])
-    # df["year"] = df["year"].astype("Int64")
-    # df["month"] = df["month"].astype("Int64")
-    # df["day"] = df["day"].astype("Int64")
-
-    return df
-
-
-def create_dictionary_of_dfs_from_paths(list_of_files: List[Path] = None, delimiter="‎"):
-    assert list_of_files, "Need to define a list of paths to create dataframe from"
-
-    # Create a dictionary of csv files per perpetrator
-    paths = dict()
-    for csv in list_of_files:
-        parent = csv.parents[0]
-        # key = parent.name
-        key = str(parent.relative_to(parent.parents[1]))
-        paths[key] = csv
-    
-    # Create a dictionary of dataframes per perpetrator
-    dfs = dict()
-    for key, value in paths.items():
-        dfs[key] = pd.read_csv(value, delimiter=delimiter, engine='python', encoding="utf-8", quoting=QUOTE_NONE)
-    
-    return dfs
-
-def _write_csv(df: pd.DataFrame, fname: str, out_dir: Path, create_dir_if_not_exists: bool = True, index: bool = False) -> bool:
-    assert fname.split(".")[-1] == "csv", "Format needs to be csv"
-    if create_dir_if_not_exists:
-        out_dir.mkdir(parents=True, exist_ok=True) # Make sure the folder exists
-    df.to_csv(out_dir / fname, sep="‎", header=True, index=False, date_format="%Y-%m-%d")
+from functools import partial
+import click
 
 def _write_formatted_xlsx(df: pd.DataFrame, fname: str, out_dir: Path, create_dir_if_not_exists: bool = True, index: bool = False) -> bool:
     assert fname.split(".")[-1] == "xlsx", "Format needs to be excel"
